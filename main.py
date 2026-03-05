e2b8067d48e3356d5215d04c621605b4c125b4d3
Gabriel Varisco Oliveira <gvarisco@uolinc.com>
2026-03-05 02:18:32 -0300

    Add Telegram message sending functionality

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
+import requests
+import os
+
+TOKEN = os.getenv("TELEGRAM_TOKEN")
+CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+
+def send_telegram(message):
+
+    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+
+    payload = {
+        "chat_id": CHAT_ID,
+        "text": message
+    }
+
+    r = requests.post(url, data=payload)
+
+    print(r.text)
+
 if __name__ == "__main__":
     send_telegram("TESTE VARISCO QUANT")
