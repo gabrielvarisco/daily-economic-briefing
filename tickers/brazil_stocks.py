2665d535c8dcf507472df327bfe98bac5b3b5c10
Gabriel Varisco Oliveira <gvarisco.oliveira@gmail.com>
2026-03-06 17:08:24 -0300

    Create brazil_stocks.py

---
+++ tickers/brazil_stocks.py
@@ -0,0 +1,14 @@
+# Lista de ações brasileiras
+
+BRAZIL_TICKERS = [
+    "VALE3.SA",
+    "PETR4.SA",
+    "ITUB4.SA",
+    "BBDC4.SA",
+    "ABEV3.SA",
+    "WEGE3.SA",
+    "BBAS3.SA",
+    "B3SA3.SA",
+    "SUZB3.SA",
+    "RENT3.SA"
+]
