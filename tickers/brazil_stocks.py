89a8232ec6a76dd30b62faebd058c11765f8ceb1
Gabriel Varisco Oliveira <gvarisco@uolinc.com>
2026-03-06 20:45:43 -0300

    Update Brazil stock tickers list
    Removed and replaced some tickers in the Brazil stock list.

--- tickers/brazil_stocks.py
+++ tickers/brazil_stocks.py
@@ -4,13 +4,10 @@ BRAZIL_TICKERS = [
     "VALE3.SA",
     "PETR4.SA",
     "ITUB4.SA",
-    "BBDC4.SA",
+    "ITSA4.SA",
     "ABEV3.SA",
-    "WEGE3.SA",
     "BBAS3.SA",
-    "B3SA3.SA",
-    "SUZB3.SA",
-    "RENT3.SA"
+    "CSAN3.SA"
 ]
 
 BRAZIL_INDEX = "^BVSP"        # IBOVESPA
